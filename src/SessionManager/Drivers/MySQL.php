e8df34fe7560527c6ba2baf635b42f8641b0d0ed
Dale <dale.phutureproof@gmail.com>
2016-09-27 21:10:39 +0100

    Docblocks

--- src/SessionManager/Drivers/MySQL.php
+++ src/SessionManager/Drivers/MySQL.php
@@ -6,22 +6,38 @@ use PhutureProof\SessionManager\Interfaces\SessionToStorage;
 
 class MySQL implements SessionToStorage
 {
-    public $database;
+    /**
+     * @var \PDO
+     */
+    private $_database;
 
+    /**
+     * MySQL constructor.
+     *
+     * @param \PDO $database
+     */
     public function __construct(\PDO $database)
     {
-        $this->database = $database;
+        $this->_database = $database;
     }
 
+    /**
+     * @return bool
+     */
     public function close()
     {
-        unset($this->database);
+        unset($this->_database);
         return true;
     }
 
+    /**
+     * @param $session_id
+     *
+     * @return bool|mixed
+     */
     public function readSessionData($session_id)
     {
-        $fetchSession = $this->database->prepare("SELECT * FROM sessions WHERE session_id = ?");
+        $fetchSession = $this->_database->prepare("SELECT * FROM sessions WHERE session_id = ?");
         if ($fetchSession->execute([$session_id])) {
             $result = $fetchSession->fetch();
 
@@ -31,9 +47,15 @@ class MySQL implements SessionToStorage
         return false;
     }
 
+    /**
+     * @param $session_id
+     * @param $session_data
+     *
+     * @return bool
+     */
     public function writeSessionData($session_id, $session_data)
     {
-        $writeSession = $this->database->prepare("REPLACE INTO sessions (session_id, session_data) VALUES (?, ?)");
+        $writeSession = $this->_database->prepare("REPLACE INTO sessions (session_id, session_data) VALUES (?, ?)");
         if ($writeSession->execute([$session_id, $session_data])) {
             return true;
         }
@@ -41,9 +63,14 @@ class MySQL implements SessionToStorage
         return false;
     }
 
+    /**
+     * @param $session_id
+     *
+     * @return bool
+     */
     public function deleteSessionData($session_id)
     {
-        $destroySession = $this->database->prepare("DELETE FROM sessions WHERE session_id = ?");
+        $destroySession = $this->_database->prepare("DELETE FROM sessions WHERE session_id = ?");
         if ($destroySession->execute([$session_id])) {
             return true;
         }
@@ -51,9 +78,14 @@ class MySQL implements SessionToStorage
         return false;
     }
 
+    /**
+     * @param $session_id
+     *
+     * @return bool
+     */
     public function checkSessionData($session_id)
     {
-        $checkSession = $this->database->prepare("SELECT * FROM sessions WHERE session_id = ?");
+        $checkSession = $this->_database->prepare("SELECT * FROM sessions WHERE session_id = ?");
         if ($checkSession->execute([$session_id])) {
             if (count($checkSession->fetchAll())) {
                 return true;
